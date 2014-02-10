296cbbc17da78e133a702a59a7807925280db250
Bretton Wade <Bretton.Wade@gmail.com>
2014-02-10 13:29:10 -0500

    Jane was hoisted to its own repos.

--- Jane/src/Filter.js
+++
@@ -1,7 +0,0 @@
-﻿//------------------------------------------------------------------------------
-// filter functions for Jane data objects
-//------------------------------------------------------------------------------
-Jane.Filter = function (base) {
-    var Filter = Object.create(base);
-    return Filter;
-}(null);
