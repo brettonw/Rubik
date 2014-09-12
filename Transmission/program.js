f4e9f721f4a4238a8fe3a05070687649b10d2c8c
Bretton Wade <bretton.wade@gmail.com>
2014-09-12 02:45:18 -0400

    - Add convalescence - convert whole simulator to using states on atoms

--- Transmission/program.js
+++ Transmission/program.js
@@ -23,13 +23,12 @@
 // We start with a population of unspecified individuals, and run a clock. At
 // each tick of the clock we conduct an event. An event represents an
 // opportunity for a disease transfer to occur. The population is seeded to
-// provide a source for the disease transfer to occur.
+// provide a source for the disease.
 
 // events model transmission rates, affected by the various strategies
 
-var pink = "#FF8080";
-var red = "#800000";
-var gray = "#808080";
+var borderColorHighlight = "#FFFF00";
+var borderColorActive = "#808080";
 
 // the population
 var populationWidth = 10;
@@ -37,23 +36,25 @@ var populationHeight = 10;
 var populationSize = populationWidth * populationHeight;
 var population;
 var infectedCount = 0;
-
-// the disease parameters
-var transmissibility = 0.5;
-var daysToIncubateMin = 7.0;
-var daysToIncubateMax = 14.0;
-var daysToHealMin = 10.0;
-var daysToHealMax = 21.0;
+var totalInfectedCount = 0;
 
 // the clock
-var clock = 0;
-var day = 0;
+var clock;
+var day;
 var clockDisplay;
-var paused = true;
+var paused;
 var individualEventsPerWeek = 2.0;
 var eventsPerDiem = Math.floor (populationSize * (individualEventsPerWeek / 7.0));
 
-var lastPersonA, lastPersonB;
+var lastAtomA, lastAtomB;
+
+// disease states with parameters
+var states = {
+    HEALTHY         : { name: "HEALTHY",      color: "#FFFFFF", susceptible: true,  contagious: 0.00 },
+    INFECTED        : { name: "INFECTED",     color: "#FFC0C0", susceptible: false, contagious: 0.05, daysMin: 7, daysMax: 14 },
+    CLINICAL        : { name: "CLINICAL",     color: "#FF0000", susceptible: false, contagious: 0.50, daysMin: 7, daysMax: 14 },
+    CONVALESCENT    : { name: "CONVALESCENT", color: "#800000", susceptible: false, contagious: 0.05 }
+};
 
 function createPopulation () {
     population = [];
@@ -62,64 +63,70 @@ function createPopulation () {
     for (var id = 0; id < populationSize; ++id) {
         population[id] = {
             id: id,
-            infectedDay: -1,
-            contagious: false
+            state: states.HEALTHY,
+            day: -1
         };
     }
 
     // now seed one of the individuals with disease
-    var a = Math.floor (Math.random () * populationSize);
-    var personA = population[a];
-    personA.infectedDay = day;
-    //personA.contagious = true;
-    infectedCount = 1;
+    var randomIndex = Math.floor (Math.random () * populationSize);
+    var atom = population[randomIndex];
+    atom.state = states.INFECTED;
+    atom.day = day;
+    infectedCount = totalInfectedCount = 1;
 };
 
 function conductEvent () {
-    // clear the last event info by denoting those persons as "active"
-    lastPersonA.link.style.stroke = gray;
-    lastPersonB.link.style.stroke = gray;
+    // clear the last event info by denoting those atoms as "active"
+    lastAtomA.link.style.stroke = borderColorActive;
+    lastAtomB.link.style.stroke = borderColorActive;
 
     // randomly pair two individuals from the population
-    var a = Math.floor (Math.random () * populationSize);
-    var b = a;
-    while (b == a) {
-        b = Math.floor (Math.random () * populationSize);
+    var indexA = Math.floor (Math.random () * populationSize);
+    var indexB = indexA;
+    while (indexB == indexA) {
+        indexB = Math.floor (Math.random () * populationSize);
     }
-    var personA = population[a];
-    var personB = population[b];
+    var atomA = population[indexA];
+    var atomB = population[indexB];
 
     // highlight this pairing
-    personA.link.style.stroke = "yellow";
-    personB.link.style.stroke = "yellow";
+    atomA.link.style.stroke = borderColorHighlight;
+    atomB.link.style.stroke = borderColorHighlight;
+
+    // check if a transmission an occur
+    var canTransmit = function (a, b) {
+        return (a.state.susceptible && (b.state.contagious > 0.0));
+    }
 
     // now handle the event between these two individuals to see if transmission
     // occurs, only if just one of them is infected and the other is not
-    if ((personA.contagious && (personB.infectedDay < 0)) ||
-        (personB.contagious && (personA.infectedDay < 0))) {
+    if (canTransmit (atomA, atomB) || canTransmit (atomB, atomA)) {
         // if the infections would transfer...
-        if (Math.random () < transmissibility) {
-            ++infectedCount;
+        if (Math.random () < (atomA.state.contagious + atomB.state.contagious)) {
+            var infectAtom = function (atom) {
+                if (atom.state == states.HEALTHY) {
+                    ++infectedCount;
+                    ++totalInfectedCount;
+                    atom.state = states.INFECTED;
+                    atom.day = day;
+                    atom.link.style.fill = atom.state.color;
+                }
+            };
 
-            // transfer the infection
-            if (personA.infectedDay < 0) {
-                personA.infectedDay = day;
-                personA.link.style.fill = pink;
-            } else {
-                personB.infectedDay = day;
-                personB.link.style.fill = pink;
-            }
+            infectAtom (atomA);
+            infectAtom (atomB);
         }
     }
 
     // save this information for the next event
-    lastPersonA = personA;
-    lastPersonB = personB;
+    lastAtomA = atomA;
+    lastAtomB = atomB;
 }
 
 function allInfected () {
-    lastPersonA.link.style.stroke = gray;
-    lastPersonB.link.style.stroke = gray;
+    lastAtomA.link.style.stroke = borderColorActive;
+    lastAtomB.link.style.stroke = borderColorActive;
 }
 
 function startNewDay () {
@@ -128,23 +135,35 @@ function startNewDay () {
     // sweep over the population to see if somebody becomes contagious or heals
     // or dies, or...
     for (var id = 0; id < populationSize; ++id) {
-        var person = population[id];
+        var atom = population[id];
 
-        // if the person is infected but not contagious, generate a probability
-        // they will become contagious
-        if ((person.infectedDay >= 0) && (!person.contagious)) {
-            var probability = ((day - person.infectedDay) - daysToIncubateMin) / (daysToIncubateMax - daysToIncubateMin);
-            if (Math.random () < probability) {
-                person.contagious = true;
-                person.link.style.fill = red;
+        switch (atom.state.name) {
+            case states.INFECTED.name: {
+                var probability = ((day - atom.day) - atom.state.daysMin) / (atom.state.daysMax - atom.state.daysMin);
+                if (Math.random () < probability) {
+                    atom.state = states.CLINICAL;
+                    atom.day = day;
+                    atom.link.style.fill = atom.state.color;
+                }
             }
+            break;
+            case states.CLINICAL.name: {
+                var probability = ((day - atom.day) - atom.state.daysMin) / (atom.state.daysMax - atom.state.daysMin);
+                if (Math.random () < probability) {
+                    --infectedCount;
+                    atom.state = states.CONVALESCENT;
+                    atom.day = day;
+                    atom.link.style.fill = atom.state.color;
+                }
+            }
+            break;
         }
     }
 }
 
 function tick () {
     if (! paused) {
-        if (infectedCount < populationSize) {
+        if ((infectedCount > 0) && (infectedCount < populationSize)) {
             // see if it's a new day
             if ((clock % eventsPerDiem) == 0) {
                 // do things that happen on a per day basis - we do this here to
@@ -163,7 +182,7 @@ function tick () {
             paused = true;
         }
     }
-    clockDisplay.textContent = "Day " + day + " (" + clock + ", " + infectedCount + "/" + populationSize + ")";
+    clockDisplay.textContent = "Day " + day + " (" + clock + ", " + totalInfectedCount + "/" + populationSize + ")";
 }
 
 function click () {
@@ -193,7 +212,7 @@ function makeSvg () {
     // loop over the whole population to place each individual in the world
     for (var id = 0; id < populationSize; ++id) {
         // get the individual from the population
-        var person = population[id];
+        var atom = population[id];
 
         // compute the position of the block
         var x = (id % populationWidth) + 1;
@@ -206,7 +225,7 @@ function makeSvg () {
         rect += "<rect id=\"rect" + id + "\"";
         rect += " x=\"" + x + "\" y=\"" + y + "\"";
         rect += " width=\"" + horizontalSize + "\" height=\"" + verticalSize + "\"";
-        rect += " fill=\"" + ((person.infectedDay >= 0) ? pink : "white") + "\"";
+        rect += " fill=\"" + atom.state.color + "\"";
         rect += " stroke=\"black\" stroke-width=\"0.005\" />"
         svg += rect;
     }
@@ -222,21 +241,25 @@ function makeSvg () {
 
 function linkSvg () {
     for (var id = 0; id < populationSize; ++id) {
-        var person = population[id];
+        var atom = population[id];
         var link = document.getElementById ("rect" + id);
-        person.link = link;
+        atom.link = link;
     }
     clockDisplay = document.getElementById ("clockDisplay");
 
-    // create a dummy person to clear status on so that we can remove an "if"
+    // create a dummy atom to clear status on so that we can remove an "if"
     // statement from the inner loop (this object will be abandoned very early)
-    lastPersonA = {};
-    lastPersonA.link = {};
-    lastPersonA.link.style = {};
-    lastPersonB = lastPersonA;
+    lastAtomA = {};
+    lastAtomA.link = {};
+    lastAtomA.link.style = {};
+    lastAtomB = lastAtomA;
 }
 
 function main () {
+    // reset the clock
+    clock = day = 0;
+    paused = true;
+
     createPopulation ();
     var display = makeSvg ();
     document.getElementById ("display").innerHTML = display;
